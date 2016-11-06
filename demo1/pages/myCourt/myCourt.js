73f4836d9fa37e84263c4f833acd7ef97665e6e0
qiaoxingrui <330565600@qq.com>
2016-11-06 20:21:22 +0800

    增加了我的课程

---
+++ demo1/pages/myCourt/myCourt.js
@@ -0,0 +1,38 @@
+//index.js
+//获取应用实例
+
+
+var app = getApp();
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {}
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    console.log('onLoad')
+    var that = this
+    //调用应用实例的方法获取全局数据
+    app.getUserInfo(function(userInfo){
+      //更新数据
+      that.setData({
+        userInfo:userInfo
+      })
+    })
+  },
+  bindDateChange: function(e) {
+    var ymd = e.detail.value;
+    var month = ymd.split("-")[1];
+    if(month.indexOf("0") == 0) {
+      month = month.substring(1,2);
+    }
+    this.setData({
+      date: month
+    })
+  }
+})
